c935511d4fa05f4a50356db48d72b3ef20046df5
gsp8181 <gsp8181@users.noreply.github.com>
2014-11-12 15:44:03 +0000

    Added a service for getting a list of flights

---
+++ src/main/java/org/jboss/quickstarts/wfk/flight/FlightRESTService.java
@@ -0,0 +1,103 @@
+/*
+ * Geoffrey Prytherch - Adapted from JBoss Examples, with the licence given below
+ * 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.quickstarts.wfk.flight;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.NoResultException;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.WebApplicationException;
+
+import org.jboss.quickstarts.wfk.customer.Customer;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * <p>This class exposes the functionality of {@link CustomerService} over HTTP endpoints as a RESTful resource via
+ * JAX-RS.</p>
+ *
+ * <p>Full path for accessing the Customer resource is rest/Customers .</p>
+ *
+ * <p>The resource accepts and produces JSON.</p>
+ * 
+ * @author Geoffrey Prytherch
+ * @see CustomerService
+ * @see javax.ws.rs.core.Response
+ */
+/*
+ * The Path annotation defines this as a REST Web Service using JAX-RS.
+ * 
+ * By placing the Consumes and Produces annotations at the class level the methods all default to JSON.  However, they 
+ * can be overridden by adding the Consumes or Produces annotations to the individual method.
+ * 
+ * It is Stateless to "inform the container that this RESTful web service should also be treated as an EJB and allow 
+ * transaction demarcation when accessing the database." - Antonio Goncalves
+ * 
+ */
+@Path("/flights")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Stateless
+public class FlightRESTService {
+    @Inject
+    private @Named("logger") Logger log;
+    
+    @Inject
+    private FlightService service;    
+    
+    /**
+     * <p>Search for and return all the Customers.  They are sorted alphabetically by name.</p>
+     * 
+     * @return A Response containing a list of Customers
+     */
+    @GET
+    public Response retrieveAllFlights() {
+        JSONArray Customers = service.findAllOrderedByName();
+        
+        if(Customers != null)
+        {
+        	return Response.ok(Customers.toString()).build();
+        } else
+        {
+        	return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+        
+    }
+    
+    
+}
---
+++ src/main/java/org/jboss/quickstarts/wfk/flight/FlightService.java
@@ -0,0 +1,97 @@
+/*
+ * Geoffrey Prytherch - Adapted from JBoss Examples, with the licence given below
+ * 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.quickstarts.wfk.flight;
+
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.jboss.quickstarts.wfk.contact.Contact;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
+import javax.ws.rs.GET;
+import javax.ws.rs.core.Response;
+
+import java.net.URI;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * <p>This Service assumes the Control responsibility in the ECB pattern.</p>
+ *
+ * <p>The validation is done here so that it may be used by other Boundary Resources. Other Business Logic would go here
+ * as well.</p>
+ *
+ * <p>There are no access modifiers on the methods, making them 'package' scope.  They should only be accessed by a
+ * Boundary / Web Service class with public methods.</p>
+ *
+ * @author Geoffrey Prytherch
+ * @see HotelValidator
+ * @see HotelRepository
+ */
+
+//@Dependent annotation designates the default scope, listed here so that you know what scope is being used.
+@Dependent
+public class FlightService {
+
+    @Inject
+    private @Named("logger") Logger log;
+	
+    @Inject
+    private @Named("httpClient") CloseableHttpClient httpClient;
+    
+    /**
+     * <p>Returns a List of all persisted {@link Contact} objects, sorted alphabetically by last name.<p/>
+     * 
+     * @return List of Contact objects
+     */
+    JSONArray findAllOrderedByName() {
+        //Perform a rest call to get the state of the contact from the allareacodes.com API
+    	try
+    	{
+        URI uri = new URIBuilder()
+                .setScheme("http")
+                .setHost("jbosscontactsangularjs-110336260.rhcloud.com")
+                .setPath("/rest/flights")
+                .build();
+        HttpGet req = new HttpGet(uri);
+        CloseableHttpResponse response = httpClient.execute(req);
+        String responseBody = EntityUtils.toString(response.getEntity());
+        JSONArray responseJson = new JSONArray(responseBody);
+        //responseJson.
+        //JSONArray areaCodes = responseJson.getJSONArray("area_codes");
+        //contact.setState(areaCodes.getJSONObject(0).getString("state"));
+        
+        HttpClientUtils.closeQuietly(response);
+        return responseJson;
+    	} catch (Exception e) {
+    	log.info(e.toString());
+    	return null;
+    	}
+    }
+}
