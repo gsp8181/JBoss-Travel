a9c4dc35e91d2a13714352f80dcb6c95c02dafd6
gsp8181 <gsp8181@users.noreply.github.com>
2014-11-12 16:01:16 +0000

    Moved TravelPlan and Flight to TravelAgent/...

--- src/main/java/org/jboss/quickstarts/wfk/travelagent/flight/FlightService.java
+++ src/main/java/org/jboss/quickstarts/wfk/travelagent/flight/FlightService.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.quickstarts.wfk.flight;
+package org.jboss.quickstarts.wfk.travelagent.flight;
 
 
 import org.apache.http.client.methods.CloseableHttpResponse;
--- src/main/java/org/jboss/quickstarts/wfk/travelagent/travelplan/TravelPlanValidator.java
+++ src/main/java/org/jboss/quickstarts/wfk/travelagent/travelplan/TravelPlanValidator.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.quickstarts.wfk.travelplan;
+package org.jboss.quickstarts.wfk.travelagent.travelplan;
 
 import java.util.HashSet;
 import java.util.Set;
