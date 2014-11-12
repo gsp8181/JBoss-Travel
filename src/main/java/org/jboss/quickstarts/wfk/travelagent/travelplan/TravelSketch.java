df1b7916f64b8183814d9d50c2a2c46370f7e0f4
gsp8181 <gsp8181@users.noreply.github.com>
2014-11-12 16:28:21 +0000

    Made a quick stub for a creator class for the travel agent

---
+++ src/main/java/org/jboss/quickstarts/wfk/travelagent/travelplan/TravelSketch.java
@@ -0,0 +1,86 @@
+/*
+ * Geoffrey Prytherch - Adapted from JBoss Examples, with the licence given below
+ *  
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.quickstarts.wfk.travelagent.travelplan;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class TravelSketch implements Serializable {
+    /** Default value included to remove warning. Remove or modify at will. **/
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @Column(name = "customerId")
+    private Long customerId;
+
+    @NotNull
+    @Column(name = "flightId")
+    private Long flightId;
+
+    @NotNull
+    @Column(name = "hotelId")
+    private Long hotelId;
+
+    @NotNull
+    @Column(name = "taxiId")
+    private Long taxiId;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(Long flightId) {
+        this.flightId = flightId;
+    }
+
+    public Long getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Long hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public Long getTaxiId() {
+        return taxiId;
+    }
+
+    public void setTaxiId(Long taxiId) {
+        this.taxiId = taxiId;
+    }
+
+}
